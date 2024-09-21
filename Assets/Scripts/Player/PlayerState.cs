97264cccc58b095ef304624da46990b355f94e09
Kkkiittt <khamidov357@gmail.com>
2024-09-21 13:04:57 +0500

    mana rec

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -7,14 +8,14 @@ public class PlayerState : ScriptableObject
 	//that refers to unlocked abilities
 	public List<BasicAbilityData> Abilities;
 	public List<TempBuffData> Buffs;
-	public int Health;
-	public int MaxHealth;
-	public int Mana;
-	public int MaxMana;
-	public int ManaRecovery;
-	public int Attack;
-	public int AttackSpeed;
-	public int Speed;
+	public float Health;
+	public float MaxHealth;
+	public float Mana;
+	public float MaxMana;
+	public float ManaRecovery;
+	public float Attack;
+	public float AttackSpeed;
+	public float Speed;
 	void Start()
 	{
 
@@ -22,6 +23,42 @@ public class PlayerState : ScriptableObject
 
 	void Update()
 	{
-
+		foreach(var buff in Buffs)
+		{
+			buff.Duration -= Time.deltaTime;
+			if(buff.Duration <= 0)
+			{
+				Buffs.Remove(buff);
+				switch(buff.Variable)
+				{
+					case Variable.Health:
+						Health -= buff.Value;
+						break;
+					case Variable.Mana:
+						Mana -= buff.Value;
+						break;
+					case Variable.Attack:
+						Attack -= buff.Value;
+						break;
+					case Variable.AttackSpeed:
+						AttackSpeed -= buff.Value;
+						break;
+					case Variable.ManaRecovery:
+						ManaRecovery -= buff.Value;
+						break;
+					case Variable.Speed:
+						Speed -= buff.Value;
+						break;
+					case Variable.MaxHealth:
+						MaxHealth -= buff.Value;
+						break;
+					case Variable.MaxMana:
+						MaxMana -= buff.Value;
+						break;
+				}
+				break;
+			}
+			Mana+=Math.Min(ManaRecovery*Time.deltaTime, MaxMana-Mana);
+		}
 	}
 }
