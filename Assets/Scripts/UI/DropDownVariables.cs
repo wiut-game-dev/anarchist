a44b7612bab21b12829ea9cbae4dbdfc5473dcf6
Askhatik <ashatbekab038@gmail.com>
2024-10-20 19:08:52 +0500

    Some stupid decision and better looking proto-UI

--- Assets/Scripts/UI/DropDownVariables.cs
+++ Assets/Scripts/UI/DropDownVariables.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 
 public class DropDownVariables : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
     public TempBuffData TempBuff;
-    public InputField input;
-    public Toggle permanent;
+    public TMP_InputField input;
+    public Toggle permanent = null;
+    public bool Current;
+    public bool Final;
+
+    public int GetValue()
+    {
+        return dropdown.value;
+    }
 
     public void Permanentce()
     {
         if(permanent.isOn)
         {
-            VariableChooserPerm();
+            VariableChooserPerm(GetValue());
         }
         else
         {
@@ -22,16 +30,38 @@ public class DropDownVariables : MonoBehaviour
         }
     }
 
-    public void VariableChooserPerm()
+    public void CurrentOrFinal()
     {
-        int variable = 0;
-        if(variable == 0) //HealthPoints
-        {  
-            
+        if(Current)
+        {
+            VariableChooserPerm(1);
+        }
+        if(Final)
+        {
+            VariableChooserPerm(0);
         }
     }
 
+    public void VariableChooserPerm(int variable)
+    {
+        switch (variable)
+        {
+            case 1: variable = 0; break;
+        }
+        
+    }
+
     public void VariableChooserTemp()
+    {
+
+    }
+
+    public void VariableChooserCurrent()
+    {
+
+    }
+
+    public void VariableChooserFinal()
     {
 
     }
