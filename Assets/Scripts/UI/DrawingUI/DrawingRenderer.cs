1e67d3e6cfb586bc072b63feb4e2f004d837c669
Askhatik <ashatbekab038@gmail.com>
2024-10-22 22:20:17 +0500

    Final changes are needed

---
+++ Assets/Scripts/UI/DrawingUI/DrawingRenderer.cs
@@ -0,0 +1,236 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+public class DrawingRenderer : MonoBehaviour
+{
+    // Texture that we render to
+    [SerializeField]
+    private RenderTexture m_renderTexture;
+    // Preview texture that we render to
+    [SerializeField]
+    private RenderTexture m_previewRenderTexture = null;
+
+    [SerializeField]
+    private Vector2Int m_textureSize = new(32, 32);
+
+    //Texture2D that we modify and next use to apply changes to the RenderTexture.
+    //We could skip RenderTexture but it separates nicely our code into separate units
+    //(this script knows nothing about UI. It only knows about the RenderTexture)
+    private Texture2D m_drawingTexture, m_previewTexture;
+
+    //Color array 
+    Color[] m_drawingPixelColors, m_previewPixelColors;
+
+    TextureRendererHelper m_textureRendererHelper;
+
+    public Vector2Int TextureSize => new(m_renderTexture.width, m_renderTexture.height);
+
+    //Brush size. I will want to move it to a separate class
+    public int BrushSize { get; private set; } = 1;
+
+    private void Awake()
+    {
+        //Set size of the texture
+        m_textureSize = ValidateSize(m_textureSize);
+        m_textureRendererHelper = new TextureRendererHelper();
+        m_renderTexture.width = m_textureSize.x;
+        m_renderTexture.height = m_textureSize.y;
+        //Ensures that we use sRGB so the colors are displayed correctly when you import our drawing to Aseprite or other sowftware
+        m_renderTexture.format = RenderTextureFormat.ARGB32;
+    }
+
+    void Start()
+    {
+
+        //Prepare the drawing texture
+        m_drawingTexture =
+            new(m_renderTexture.width, m_renderTexture.height, TextureFormat.RGBA32, false);
+        //Ensures that preview texture is the same size as RenderTexture
+        m_previewRenderTexture.width = m_renderTexture.width;
+        m_previewRenderTexture.height = m_renderTexture.height;
+        //Color format is important for saving to png
+        m_previewRenderTexture.format = m_renderTexture.format;
+        m_previewTexture =
+            new(m_previewRenderTexture.width, m_previewRenderTexture.height, TextureFormat.RGBA32, false);
+        m_drawingTexture.filterMode = FilterMode.Point;
+        m_drawingPixelColors = new Color[m_renderTexture.width * m_renderTexture.height];
+        m_previewPixelColors = new Color[m_previewRenderTexture.width * m_previewRenderTexture.height];
+        ClearCanvas();
+        ClearTexture(m_previewPixelColors, m_previewRenderTexture, m_previewTexture);
+    }
+
+    private Vector2Int ValidateSize(Vector2Int m_textureSize)
+    {
+        if (m_textureSize.x < 1 || m_textureSize.y < 1)
+        {
+            Debug.LogError("Texture size must be at least 1x1. Setting to 16x16.");
+            return new(16, 16);
+        }
+        return m_textureSize;
+    }
+
+    private void UpdateRenderTexture(RenderTexture renderTexture, Texture2D textureToUpdate, Color[] pixelsToApply)
+    {
+        //Apply colors to our Texture2D
+        textureToUpdate.SetPixels(pixelsToApply);
+        //We need to call Apply to Texture2D before we print its content to the RenderTexture
+        textureToUpdate.Apply();
+
+        m_textureRendererHelper.UpdateRenderTexture(renderTexture, textureToUpdate);
+    }
+
+    /// <summary>
+    /// Draws new pixels on the canvas
+    /// </summary>
+    /// <param name="pixelPositions"></param>
+    /// <param name="color"></param>
+    /// <param name="blending"></param>
+    public void DrawOnCanvas(List<Vector2Int> pixelPositions, Color color, bool blending = true)
+    {
+        DrawOnTexture(color, pixelPositions, m_renderTexture, m_drawingTexture, m_drawingPixelColors, blending);
+    }
+
+    /// <summary>
+    /// Draws new pixels on the preview Texture
+    /// </summary>
+    /// <param name="pixelPositions"></param>
+    /// <param name="color"></param>
+    /// <param name="blending"></param>
+    public void DrawPreview(List<Vector2Int> pixelPositions, Color color, bool blending = true)
+    {
+        for (int i = 0; i < m_previewPixelColors.Length; i++)
+        {
+            m_previewPixelColors[i] = Color.clear;
+        }
+        DrawOnTexture(color, pixelPositions, m_previewRenderTexture, m_previewTexture, m_previewPixelColors, blending);
+    }
+
+    /// <summary>
+    /// Allows us to either blend the new color with the existing one or override it. Eraser removed the existing color while brush and other tools blend 
+    /// colors (a full alpha color will completely override the existing color).
+    /// We pass so many parameters because I reuse this for preview and canvas drawing.
+    /// </summary>
+    /// <param name="color">Color to paint</param>
+    /// <param name="pixelPositionsInTextureSpace"> Position in RenderTexture space (ex 32x32 mean x can be 0-31)</param>
+    /// <param name="renderTexture">RenderTexture to update</param>
+    /// <param name="textureToUpdate">Intermediary Texture2D to update</param>
+    /// <param name="colors">Colors array to update</param>
+    /// <param name="blending"></param>
+    private void DrawOnTexture(Color color, List<Vector2Int> pixelPositionsInTextureSpace, RenderTexture renderTexture, Texture2D textureToUpdate, Color[] colors, bool blending)
+    {
+        //apply brush size
+        foreach (Vector2Int pos in pixelPositionsInTextureSpace)
+        {
+            if (blending)
+                BlendPixel(pos, color, renderTexture.width, colors);
+            else
+                DrawPixel(pos, color, renderTexture.width, colors);
+        }
+
+        UpdateRenderTexture(renderTexture, textureToUpdate, colors);
+    }
+
+    /// <summary>
+    /// Blends pixel color present on the texture with the newly painted color
+    /// </summary>
+    /// <param name="position">Position in RenderTexture space (ex 32x32 mean x can be 0-31)</param>
+    /// <param name="newColor"></param>
+    /// <param name="width">Texture width</param>
+    /// <param name="pixelColors">Color array to affect</param>
+    private void BlendPixel(Vector2Int position, Color newColor, int width, Color[] pixelColors)
+    {
+        // Calculate 1D array index (row-major order)
+        int pixelIndex = position.y * width + position.x;
+
+        // Get the existing color
+        Color existingColor = pixelColors[pixelIndex];
+
+        // Blend the new color with the existing color
+        Color blendedColor = BlendColors(existingColor, newColor);
+
+        // Update color array with the blended color
+        pixelColors[pixelIndex] = blendedColor;
+    }
+
+    /// <summary>
+    /// Blends 2 colors together
+    /// </summary>
+    /// <param name="background"></param>
+    /// <param name="foreground"></param>
+    /// <returns></returns>
+    private Color BlendColors(Color background, Color foreground)
+    {
+        float alpha = foreground.a;
+        float inverseAlpha = 1 - alpha;
+
+        return new Color(
+            foreground.r * alpha + background.r * inverseAlpha,
+            foreground.g * alpha + background.g * inverseAlpha,
+            foreground.b * alpha + background.b * inverseAlpha,
+            Mathf.Clamp01(background.a + foreground.a)
+        );
+    }
+
+    /// <summary>
+    /// Draws a single pixel on the canvas - overriding the existing color
+    /// </summary>
+    /// <param name="position">Position in RenderTexture space (ex 32x32 mean x can be 0-31)</param>
+    /// <param name="newColor"></param>
+    /// <param name="width">Texture width</param>
+    /// <param name="pixelColors">Color array to affect</param>
+    private void DrawPixel(Vector2Int position, Color color, int width, Color[] pixelColors)
+    {
+        // Calculate 1D array index (row-major order)
+        int pixelIndex = position.y * m_renderTexture.width + position.x;
+        //Update color array and the texture
+        pixelColors[pixelIndex] = color;
+    }
+
+    /// <summary>
+    /// Clears the RenderTextures that represents the canvas
+    /// </summary>
+    public void ClearCanvas()
+    {
+        ClearTexture(m_drawingPixelColors, m_renderTexture, m_drawingTexture);
+    }
+
+    /// <summary>
+    /// Clears the RenderTextures that represents the preview
+    /// </summary>
+    public void ClearPreview()
+    {
+        ClearTexture(m_previewPixelColors, m_previewRenderTexture, m_previewTexture);
+    }
+
+    private void ClearTexture(Color[] colors, RenderTexture renderTexture, Texture2D updateTexture)
+    {
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.clear;
+        }
+        UpdateRenderTexture(renderTexture, updateTexture, colors);
+    }
+
+    public void SetBrushSize(int v)
+    {
+        BrushSize = Mathf.Clamp(v, 1, Mathf.Max(m_renderTexture.width, m_renderTexture.height));
+    }
+
+    /// <summary>
+    /// Uses a normalized position 0-1 (sent by the UI) to calculate the position on the RenderTexture.
+    /// 
+    /// </summary>
+    /// <param name="normalizedPixelPosition"></param>
+    /// <returns></returns>
+    public Vector2Int CalculateTexturePosition(Vector2 normalizedPixelPosition)
+    {
+        return TextureRendererHelper.CalculateTexturePosition(normalizedPixelPosition, m_renderTexture);
+    }
+
+    internal Color GetPixelColor(Vector2Int pixelPosition)
+    {
+        int pixelIndex = pixelPosition.y * m_renderTexture.width + pixelPosition.x;
+        return m_drawingPixelColors[pixelIndex];
+    }
+}
