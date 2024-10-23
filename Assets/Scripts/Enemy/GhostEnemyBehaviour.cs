020928421ab0c40b2deebd42b492519fb9b2ee5b
Kkkiittt <khamidov357@gmail.com>
2024-10-23 21:26:16 +0500

    i mean it is working

--- Assets/Scripts/Enemy/GhostEnemyBehaviour.cs
+++ Assets/Scripts/Enemy/GhostEnemyBehaviour.cs
@@ -1,7 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
 
 using UnityEngine;
 
-public class KnightEnemyBehaviour : MonoBehaviour
+public class GhostEnemyBehaviour : MonoBehaviour
 {
 
 	public EnemyVisibility enemyVisibility;
@@ -12,7 +14,16 @@ public class KnightEnemyBehaviour : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		state.Health = 50;
+		state.MinArea = 5;
+		state.MaxArea = 10;
+		state.Speed = 50;
+		state.Attack = 20;
+		state.AttackSpeed = 1;
+		state.SightDistance = 20;
+		state.Activity = EnemyActivity.Idle;
+		state.WaitTime = 2;
+		state.WaitTimeCurrent = 0;
 	}
 
 	// Update is called once per frame
