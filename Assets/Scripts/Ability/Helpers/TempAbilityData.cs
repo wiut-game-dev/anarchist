3e7a1dc64872f52b73678b9699b4afb7e3944643
Askhatik <ashatbekab038@gmail.com>
2024-10-25 13:30:57 +0500

    MMM

--- Assets/Scripts/Ability/Helpers/TempAbilityData.cs
+++ Assets/Scripts/Ability/Helpers/TempAbilityData.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+
+[CreateAssetMenu(menuName = "TempAbilityData", fileName = "TempAbilityData")]
 public class TempAbilityData : ScriptableObject
 {
 	public AbilityType AbilityType;
